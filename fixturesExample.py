dac04054d830175d19fbc625f25c7d5b67dcb4cd
Elad Ben Atar <elad1593210@gmail.com>
2022-01-30 22:37:48 +0200

    Fixture example, in this example we are using fixutre + yield+ "-s" flag in the pytest command, in order to create pre and post conditions

--- fixturesExample.py
+++ fixturesExample.py
@@ -4,15 +4,14 @@ import pytest
 def setup():
     ''''this function will run before each test that gets it as arguemnt'''
     print("start browser")
+    yield
+    print("close browser")
 
 def test_1(setup):
     print("test 1 executed")
-    print("close browser")
 
 def test_2(setup):
     print("test 2 executed")
-    print("close browser")
 
 def test_3(setup):
     print("test 3 executed")
-    print("close browser")
